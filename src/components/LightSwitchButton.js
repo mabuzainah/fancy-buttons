12b8efc35ad94e154f2c23282021ebb395d7493b
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-12 08:34:42 -0500

    Update LightSwitchButton.js
    Added useState hook from React, to allow to use the state of the button and display differently based off state of the button.
    
    Utilizing the state of the "light" to toggle text on the button.
    
    Setting default state as "off"

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,10 +1,22 @@
 import React from 'react'; //optional
+// We want to be able to click on each button and change its state. 
+//To achieve this, we need to import the useState() hook in each of the button components and call it.
+import { useState } from "react";
 
 function LightSwitchButton() {
+// hooks are not inside conditionals, loops, or other functions, 
+//they need to be at the top of the component function.
+//Calling the useState() hook will return an array containing two values:
+    //a reference to get the current value of the state
+    //a setter function (i.e. a way to set the reference and re-render the app)
+
+
+const [light, setLight] = useState('off');
+
   return (
     <button className="LightSwitchButton">
-      {/* When the state is on */}<span className="on"><i>💡</i> I'm on!</span>
-      {/* When the state is off */}<span className="off"><i>💡</i> I'm off!</span>
+      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
