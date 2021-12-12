db36e3834ba4f7a2c2ba641b51a1ab7e537d1383
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-12 08:23:14 -0500

    Update CounterButton.js
    Utilizing State from React, and then specifying how many times the button is clicked based off "clickAmount" returned from state.

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,9 +1,20 @@
 import React from 'react'; //optional
+// We want to be able to click on each button and change its state. 
+//To achieve this, we need to import the useState() hook in each of the button components and call it.
+import { useState } from "react";
 
 function CounterButton() {
+// hooks are not inside conditionals, loops, or other functions, 
+//they need to be at the top of the component function. 
+//Calling the useState() hook will return an array containing two values:
+    //a reference to get the current value of the state
+    //a setter function (i.e. a way to set the reference and re-render the app)
+
+const [clickAmount, setClickAmount] = useState();
+
   return (
     <button className="CounterButton">
-      You clicked me X amount of times
+      You clicked me {clickAmount} amount of times
     </button>
   );
 }
