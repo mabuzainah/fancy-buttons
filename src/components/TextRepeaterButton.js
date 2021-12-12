832893c4dc986b62b33b2f5c3eb5c7a8cc293611
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-12 08:37:37 -0500

    Update TextRepeaterButton.js
    Using useState hook from React to store state and set state for the button.
    
    Created an array that will be responsible for storing the number of times "I like this text" is repeated and rendered as a span element.
    
    Note since we will be displaying the array in React, a key value is required and hence why it's placed in the html tag for <span>

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,9 +1,27 @@
 import React from 'react'; //optional
+// We want to be able to click on each button and change its state. 
+//To achieve this, we need to import the useState() hook in each of the button components and call it.
+import { useState } from "react";
 
 function TextRepeaterButton() {
+// hooks are not inside conditionals, loops, or other functions, 
+//they need to be at the top of the component function.
+//Calling the useState() hook will return an array containing two values:
+    //a reference to get the current value of the state
+    //a setter function (i.e. a way to set the reference and re-render the app)
+
+
+const [repetition, setRepition] = useState(1);
+
+const repititionArray = [];
+for (let i = 0; i < repetition; i++) {
+    repititionArray.push(<span key={i}>I like this text</span>);
+    // reason why we did this as rendering arrays in React we require a key value for each child of an array. 
+}
+
   return (
     <button className="TextRepeaterButton">
-      <span>I like this text</span>
+      {repititionArray}
     </button>
   );
 }
