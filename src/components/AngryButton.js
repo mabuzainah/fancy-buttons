800e360a18a269410e42f5c579a675f1fb282328
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-12-12 08:22:00 -0500

    Update AngryButton.js
    Added useState hook from React and utilizing it to set the color of the button's background based off the value for "anger"

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,11 +1,23 @@
 import React from 'react'; //optional
+// We want to be able to click on each button and change its state. 
+//To achieve this, we need to import the useState() hook in each of the button components and call it.
+import { useState } from "react";
 
 function AngryButton() {
+// hooks are not inside conditionals, loops, or other functions, 
+//they need to be at the top of the component function. 
+//Calling the useState() hook will return an array containing two values:
+    //a reference to get the current value of the state
+    //a setter function (i.e. a way to set the reference and re-render the app)
+
+const [anger, setAnger] = useState();
+
   return (
-    <button className="AngryButton">
-      {/* If you have NOT reached the maximum */}<span>Don't click me too much! </span>
-      {/* If you HAVE reached the maximum */}<span>Rawr!</span>
+    <button style={{backgroundColor: `rgba(255,0,0,${anger})`}} className="AngryButton">
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {anger > 1 && <span>Rawr!</span>}
     </button>
+
   );
 }
 
